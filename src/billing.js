5cca356063ecb197f90802981f914ec387966a5b
grin <stanislaw.grin@gmail.com>
2017-05-11 10:17:38 +0300

    feat (billing) getPurchases and allocateProduct api

--- src/billing.js
+++ src/billing.js
@@ -19,5 +19,13 @@ export default req => ({
 
   getCreditCard(appId) {
     return req.get(`${urls.billing(appId)}/creditcard`)
-  }
+  },
+
+  getPurchases(appId) {
+    return req.get(`${urls.billing(appId)}/marketplace/purchases`)
+  },
+
+  allocateProduct(appId, productId, options) {
+    return req.post(`${urls.billing(appId)}/marketplace/purchases/${productId}`, options)
+  },
 })
