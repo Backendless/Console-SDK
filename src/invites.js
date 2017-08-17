bc4008ada88f6a35bff89401133094311b80e96b
grin <stanislaw.grin@gmail.com>
2017-08-17 12:45:03 +0300

    change routes

--- src/invites.js
+++ src/invites.js
@@ -1,26 +1,11 @@
 import urls from './urls'
 
-// TODO: remove this and uncomment next lines when server-side will be ready to provide routes for invites
-const data = [{
-  email : 'foo@bar.com',
-  status: 'Registered',
-  credit: ''
-}, {
-  email : 'bar@baz.com',
-  status: 'Not Registered',
-  credit: ''
-}, {
-  email : 'bar@foo.com',
-  status: 'Upgraded',
-  credit: '$10.00 (issued on 02/10/2017)'
-}]
-
 export default req => ({
-  getInvitesStatus(/*appId*/) {
-    return Promise.resolve(data) //req.get(`${urls.appConsole(appId)}/invites-status`)
+  getStatus(appId) {
+    return req.get(`${urls.appConsole(appId)}/referrals`)
   },
 
-  sendInvite(/*appId, data*/) {
-    return Promise.resolve() // req.post(`${urls.appConsole(appId)}/send-invite`, data)
+  send(appId, data) {
+    return req.post(`${urls.appConsole(appId)}/referrals`, data)
   }
 })
