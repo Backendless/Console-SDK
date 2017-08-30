678c4f051e780c8d42fab17a63b2b5c61f0c0986
grin <stanislaw.grin@gmail.com>
2017-08-30 16:54:32 +0300

    licence methods

--- src/licence.js
+++ src/licence.js
@@ -1,16 +1,9 @@
-const tempResponse = {
-  mode     : 0,
-  licenseId: 123456,
-  expDate  : '1505199074531',
-  nodes    : 2
-}
-
 export default req => ({
   get() {
-    return Promise.resolve(tempResponse) //req.get('/console/licence')
+    return req.get('/console/licence')
   },
 
   upload(file) {
-    return Promise.resolve(tempResponse) //req.put('/console/licence', file)
+    return req.put('/console/licence', file)
   }
 })
