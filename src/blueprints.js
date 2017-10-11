ca45cf09b20349cfac01ee12abd7f1287004f200
grin <stanislaw.grin@gmail.com>
2017-10-11 23:56:47 +0300

    add id to query for blueprints

--- src/blueprints.js
+++ src/blueprints.js
@@ -1,7 +1,7 @@
 import urls from './urls'
 
 export default req => ({
-  get: function() {
-    return req.get(urls.blueprints())
+  get: function(id) {
+    return req.get(urls.blueprints()).query({ id })
   }
 })
