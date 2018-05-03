f84dfba766a9a9b8f4e33c2b203f76db25933e7b
Vladmir Upirov <vladimir.upirov@themidnightcoders.com>
2018-05-03 15:17:24 +0200

    - add methods for Generation DataTable API Docs

--- src/urls.js
+++ src/urls.js
@@ -119,6 +119,10 @@ export const dataConnectorTableEntries = (appId, connectorId, tableName) => `${d
 export const dataConnectorStoredProcedures = (appId, connectorId) => `${dataConnector(appId, connectorId)}/storedprocs`
 export const dataConnectorStoredProcedureExecution = (appId, connectorId, procedureId) => `${dataConnectorStoredProcedures(appId, connectorId)}/${procedureId}/execution`
 
+export const apiDocs = appId => `${appConsole(appId)}/api-docs`
+export const apiDocsData = appId => `${apiDocs(appId)}/data`
+export const apiDocsDataTable = (appId, tableName) => `${apiDocsData(appId)}/table/${tableName}`
+
 export default {
   appConsole,
   appInfo,
@@ -172,4 +176,6 @@ export default {
   dataConnectorTableEntries,
   dataConnectorStoredProcedures,
   dataConnectorStoredProcedureExecution,
+  apiDocsData,
+  apiDocsDataTable,
 }
