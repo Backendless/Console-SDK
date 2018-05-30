4047ff9a73f8384c171bbf270e7f80f024b3748b
Stanislaw Grin <stanislaw.grin@gmail.com>
2018-05-30 11:46:17 +0300

    feat (BKNDLSS-17027) add Cache methods

---
+++ src/cache.js
@@ -0,0 +1,23 @@
+import urls from './urls'
+
+export default req => ({
+  get(appId, pagesize, offset) {
+    return req.get(urls.cache(appId)).query({ pagesize, offset })
+  },
+
+  count(appId) {
+    return req.get(`${urls.cache(appId)}/count`)
+  },
+
+  put(appId, key, value, timeout) {
+    return req.put(urls.cache(appId, key), value).query({ timeout }).type('application/json')
+  },
+
+  updateTTL(appId, key, timeout) {
+    return req.put(`${urls.cache(appId, key)}/expireIn`).query({ timeout })
+  },
+
+  remove(appId, key) {
+    return req.delete(urls.cache(appId, key))
+  }
+})
