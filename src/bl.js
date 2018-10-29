2b0f56dd52977bea204b3573ed7bfd84b52c6681
Vladmir Upirov <vladimir.upirov@themidnightcoders.com>
2018-10-29 10:29:35 +0100

    - add "sync=true" by default for saving DRAFT Files

--- src/bl.js
+++ src/bl.js
@@ -194,8 +194,8 @@ export default req => ({
     return req.get(urls.blDraft(appId, language, model))
   },
 
-  saveDraftFiles(appId, language, model, files) {
-    return req.put(urls.blDraft(appId, language, model), files)
+  saveDraftFiles(appId, language, model, files, sync = true) {
+    return req.put(urls.blDraft(appId, language, model), files).query({ sync })
   },
 
   deployDraftFiles(appId, language, model, files, sync = true) {
