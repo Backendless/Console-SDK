1a48ed10427940551526bdfa5acb74a37b4497bd
Vladimir Upirov <vladimir.upirov@themidnightcoders.com>
2019-09-09 13:15:49 +0200

    - remove api for getting icons for ui-builder - v2.7.19-next-3

--- src/ui-builder.js
+++ src/ui-builder.js
@@ -9,7 +9,6 @@ const routes = {
 
   containers              : 'ui-builder/containers',
   container               : 'ui-builder/containers/:containerName',
-  containerIcons          : 'ui-builder/containers/:containerName/icons',
   containerPages          : 'ui-builder/containers/:containerName/pages',
   containerPage           : 'ui-builder/containers/:containerName/pages/:pageName',
   containerPageUI         : 'ui-builder/containers/:containerName/pages/:pageName/ui',
@@ -66,10 +65,6 @@ export default req => ({
 
   createContainer(appId, container) {
     return req.post(routes.containers(appId), container)
-  },
-
-  getIcons(appId, containerName) {
-    return req.get(routes.containerIcons(appId, containerName))
   },
 
   getPages(appId, containerName) {
