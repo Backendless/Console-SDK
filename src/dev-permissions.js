df35bc80e1f26fc0c375e985a4816962908f28b8
Igor Bogunkov <igor@themidnightcoders.com>
2020-05-18 18:57:26 +0300

    upd - bind dev permissions to ids

--- src/dev-permissions.js
+++ src/dev-permissions.js
@@ -7,109 +7,109 @@ const AccessTypes = {
 
 const OperationsTypes = {
   /** App Access Security */
-  INVITE_OR_REMOVE_TEAM_MEMBER : 'Add/remove developer',
-  ASSIGN_TEAM_MEMBER_PERMISSION: 'Modify permission',
+  INVITE_OR_REMOVE_TEAM_MEMBER : 1, //Add/remove developer
+  ASSIGN_TEAM_MEMBER_PERMISSION: 3, //Modify permission
 
   /** App Settings */
-  APP_RESET                 : 'Application reset',
-  MODIFY_CORS_DOMAIN_CONTROL: 'Modify CORS/domain control',
-  MODIFY_CUSTOM_DOMAIN      : 'Modify custom domain',
-  MODIFY_EMAIL_SETTINGS     : 'Modify email settings',
-  MODIFY_LIMIT_NOTIFICATION : 'Modify limit notification',
-  MODIFY_MOBILE_SETTINGS    : 'Modify mobile settings',
-  MODIFY_SOCIAL_SETTINGS    : 'Modify social settings',
-  DELETE_APPLICATION        : 'Delete application',
-  ENABLE_DISABLE_GIT        : 'Enable/disable git',
-  INSTALL_LICENSE           : 'Install license',
-  MANAGE_API_KEYS           : 'Manage API keys',
-  PROMOTE_APPLICATION       : 'Promote schema from one of your applications to another',
+  APP_RESET                 : 13, //Application reset
+  MODIFY_CORS_DOMAIN_CONTROL: 8, //Modify CORS/domain control
+  MODIFY_CUSTOM_DOMAIN      : 9, //Modify custom domain
+  MODIFY_EMAIL_SETTINGS     : 7, //Modify email settings
+  MODIFY_LIMIT_NOTIFICATION : 14, //Modify limit notification
+  MODIFY_MOBILE_SETTINGS    : 6, //Modify mobile settings
+  MODIFY_SOCIAL_SETTINGS    : 5, //Modify social settings
+  DELETE_APPLICATION        : 11, //Delete application
+  ENABLE_DISABLE_GIT        : 10, //Enable/disable git
+  INSTALL_LICENSE           : undefined, //TODO Install license
+  MANAGE_API_KEYS           : 4, //Manage API keys
+  PROMOTE_APPLICATION       : undefined, //Promote schema from one of your applications to another
 
   /** Billing */
   BILLING_SECTION            : 'Billing section',
-  ADD_UPDATE_CREDIT_CARD     : 'Add/update credit card',
-  MODIFY_BILLING_PLAN        : 'Modify billing plan',
-  MARKETPLACE_DELETE_PURCHASE: 'Delete Marketplace purchases',
-  MARKETPLACE_PURCHASE       : 'Purchase from the Marketplace',
+  ADD_UPDATE_CREDIT_CARD     : 20, //Add/update credit card
+  MODIFY_BILLING_PLAN        : 21, //Modify billing plan
+  MARKETPLACE_DELETE_PURCHASE: 23, //Delete Marketplace purchases
+  MARKETPLACE_PURCHASE       : 22, //Purchase from the Marketplace
 
   /** Business Logic */
   BUSINESS_LOGIC_SECTION : 'Business Logic section',
-  DELETE_FROM_MARKETPLACE: 'Delete from marketplace',
-  INVOKE_EVENT_WITH_MODEL: 'Invoke event with model',
-  MODIFY_BL              : 'Modify Business Logic',
-  PUBLISH_TO_MARKETPLACE : 'Publish to Marketplace',
-  RUN_TIMER              : 'Run timer',
+  DELETE_FROM_MARKETPLACE: 98, //Delete from marketplace
+  INVOKE_EVENT_WITH_MODEL: 97, //Invoke event with model
+  MODIFY_BL              : 93, //Modify Business Logic
+  PUBLISH_TO_MARKETPLACE : 94, //Publish to Marketplace
+  RUN_TIMER              : 96, //Run timer
 
   /** Data Service */
   DATA_SERVICE_SECTION                  : 'Data Service section',
-  MODIFY_DYNAMIC_SCHEMA_DEFINITION      : 'Modify "Dynamic Schema Definition" configuration',
-  CREATE_MODIFY_DELETE_COLUMN           : 'Create/Rename/Delete columns',
-  CREATE_MODIFY_DELETE_TABLE            : 'Create/Rename/Delete tables',
-  CREATE_DELETE_UPDATE_OBJECTS          : 'Create/delete/update objects',
-  CREATE_MODIFY_DELETE_TABLE_PERMISSIONS: 'Manage permissions for tables (including ACL)',
-  CREATE_MODIFY_DELETE_OWNER_PERMISSIONS: 'Manage global owner permissions',
+  MODIFY_DYNAMIC_SCHEMA_DEFINITION      : 47, //Modify "Dynamic Schema Definition" configuration
+  CREATE_MODIFY_DELETE_COLUMN           : 42, //Create/Rename/Delete columns
+  CREATE_MODIFY_DELETE_TABLE            : 40, //Create/Rename/Delete tables
+  CREATE_DELETE_UPDATE_OBJECTS          : 46, //Create/delete/update objects
+  CREATE_MODIFY_DELETE_TABLE_PERMISSIONS: 44, //Manage permissions for tables (including ACL)
+  CREATE_MODIFY_DELETE_OWNER_PERMISSIONS: 54, //Manage global owner permissions
 
   /** Data connector */
   DATA_CONNECTOR_SECTION             : 'Data Connector section',
-  CREATE_MODIFY_DELETE_DATA_CONNECTOR: 'Manage data connectors',
-  STORED_PROCEDURE                   : 'Call stored procedure',
+  CREATE_MODIFY_DELETE_DATA_CONNECTOR: 99, //Manage data connectors
+  STORED_PROCEDURE                   : 102, //Call stored procedure
 
   /** Email Templates */
   EMAIL_TEMPLATES_SECTION      : 'Email Templates section',
-  CREATE_MODIFY_DELETE_TEMPLATE: 'Manage email templates. Send test email',
+  CREATE_MODIFY_DELETE_TEMPLATE: 33, //Manage email templates. Send test email
 
   /** File Service */
   FILES_SECTION               : 'Files section',
-  MODIFY_FILE_PERMISSIONS     : 'Manage directories/files permissions',
-  MANAGE_DIRECTORIES_AND_FILES: 'Manage directories and files',
-  UPLOAD_CREATE_FILES         : 'Upload/create files',
-  VIEW_DIRECTORY_CONTENT      : 'View directory content',
+  MODIFY_FILE_PERMISSIONS     : 60, //Manage directories/files permissions
+  MANAGE_DIRECTORIES_AND_FILES: 57, //Manage directories and files
+  UPLOAD_CREATE_FILES         : 59, //Upload/create files
+  VIEW_DIRECTORY_CONTENT      : 106, //View directory content
 
   /** Geolocation */
   GEOLOCATION_SECTION              : 'Geolocation section',
-  MODIFY_GEO_CATEGORY_PERMISSIONS  : 'Manage user/role permissions for geo category',
-  CREATE_MODIFY_DELETE_GEOFENCE    : 'Create/Rename/Delete geofences',
-  MODIFY_GEO_POINT_METADATA        : 'Modify geopoint metadata',
-  CREATE_MODIFY_DELETE_GEO_CATEGORY: 'Create/Rename/Delete geo category',
-  CREATE_MODIFY_DELETE_GEO_POINT   : 'Create/Modify/Delete/Copy geo points',
+  MODIFY_GEO_CATEGORY_PERMISSIONS  : 83, //Manage user/role permissions for geo category
+  CREATE_MODIFY_DELETE_GEOFENCE    : 85, //Create/Rename/Delete geofences
+  MODIFY_GEO_POINT_METADATA        : 84, //Modify geopoint metadata
+  CREATE_MODIFY_DELETE_GEO_CATEGORY: 81, //Create/Rename/Delete geo category
+  CREATE_MODIFY_DELETE_GEO_POINT   : 88, //Create/Modify/Delete/Copy geo points
 
   /** Import/Export */
   IMPORT_EXPORT_SECTION: 'Import/Export section',
-  CLONE_APP            : 'Clone application',
-  EXPORT_APP           : 'Export app settings/app data',
-  IMPORT               : 'Import',
+  CLONE_APP            : 27, //Clone application
+  EXPORT_APP           : 24, //Export app settings/app data
+  IMPORT               : 25, //Import
 
   /** Landing Page */
   LANDING_PAGE_SECTION: 'Landing Page section',
-  MODIFY_LANDING_PAGE : 'Modify landing page',
-  PUBLISH_LANDING_PAGE: 'Publish landing page',
+  MODIFY_LANDING_PAGE : 29, //Modify landing page
+  PUBLISH_LANDING_PAGE: 30, //Publish landing page
 
   /** Log Management */
   LOG_MANAGEMENT_SECTION : 'Log Management section',
-  MODIFY_LOG_CONFIG      : 'Modify logging configuration',
-  MODIFY_LOG_INTEGRATIONS: 'Modify logging integrations',
-  DELETE_LOGGERS         : 'Delete log files',
+  MODIFY_LOG_CONFIG      : 17, //Modify logging configuration
+  MODIFY_LOG_INTEGRATIONS: 19, //Modify logging integrations
+  DELETE_LOGGERS         : 18, //Delete log files
 
   /** Messaging/Push Notifications */
   MESSAGING_SECTION                   : 'Messaging section',
-  MODIFY_MESSAGING_CHANNEL_PERMISSIONS: 'Manage user/role permissions for messaging channel',
-  CREATE_MODIFY_DELETE_BUTTON         : 'Create/Rename/Delete button options',
-  CREATE_MODIFY_DELETE_CHANEL_OPTIONS : 'Create/Rename/Delete channel options',
-  CREATE_MODIFY_DELETE_CHANEL         : 'Create/Rename/Delete messaging channels',
-  CREATE_MODIFY_DELETE_PUSH_TEMPLATE  : 'Create/Rename/Delete push template',
-  DELETE_MESSAGING_CHANNEL_PERMISSIONS: 'Delete messaging channel permissions',
-  SEND_MESSAGE_TO_CHANNEL             : 'Send a message to a channel',
-  SEND_PUSH_BY_TEMPLATE               : 'Send push notification with template',
+  MODIFY_MESSAGING_CHANNEL_PERMISSIONS: 70, //Manage user/role permissions for messaging channel
+  CREATE_MODIFY_DELETE_BUTTON         : 73, //Create/Rename/Delete button options
+  CREATE_MODIFY_DELETE_CHANEL_OPTIONS : 75, //Create/Rename/Delete channel options
+  CREATE_MODIFY_DELETE_CHANEL         : 67, //Create/Rename/Delete messaging channels
+  CREATE_MODIFY_DELETE_PUSH_TEMPLATE  : 71, //Create/Rename/Delete push template
+  DELETE_MESSAGING_CHANNEL_PERMISSIONS: undefined, //TODO Delete messaging channel permissions
+  SEND_MESSAGE_TO_CHANNEL             : 39, //Send a message to a channel
+  SEND_PUSH_BY_TEMPLATE               : 77, //Send push notification with template
 
   /** Security Roles */
   SECURITY_ROLES_SECTION : 'Security Roles section',
-  ADD_MODIFY_ASSIGN_ROLE : 'Manage security roles',
-  ASSIGN_ROLE_PERMISSIONS: 'Map users to roles',
+  ADD_MODIFY_ASSIGN_ROLE : 37, //Manage security roles
+  ASSIGN_ROLE_PERMISSIONS: 39, //Map users to roles
 
   /** User Management */
   USERS_SECTION                 : 'Users section',
-  MODIFY_LOGIN_PROPS            : 'Modify login properties',
-  MODIFY_USER_REGISTRATION_PROPS: 'Modify user registration properties',
-  LOGOUT_ALL_USERS              : 'Logout all users',
+  MODIFY_LOGIN_PROPS            : 32, //Modify login properties
+  MODIFY_USER_REGISTRATION_PROPS: 31, //Modify user registration properties
+  LOGOUT_ALL_USERS              : 107, //Logout all users
 }
 
 const OperationsLabels = {}
