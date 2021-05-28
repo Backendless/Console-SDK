7dbe477535b38eab1ff3202ebb243d41498c9fc9
Fr3ud <oo.pavelko@gmail.com>
2021-05-28 14:35:49 +0300

    Revert "BKNDLSS-24763 Add assignCustomDomain action"
    This reverts commit c80e9679df1d3e2e6e183dfe581c5bd2d168d97a.

--- src/settings.js
+++ src/settings.js
@@ -75,10 +75,6 @@ export default req => ({
     return req.post(`${urls.appConsole(appId)}/settings/custom-domain`, domainData)
   },
 
-  assignCustomDomain(appId, domainData) {
-    return req.post(`${urls.appConsole(appId)}/settings/custom-domain/assign-generated-domain`, domainData)
-  },
-
   changeCustomDomain(appId, domainData) {
     return req.put(`${urls.appConsole(appId)}/settings/custom-domain/${domainData.id}`, domainData)
   },
