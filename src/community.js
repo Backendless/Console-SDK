b518e770281875481830b79984320766835e407a
vladimir.upirov <valodya.upirov@gmail.com>
2021-07-14 12:50:31 +0300

    - add API for registering activity when new product was installed

--- src/community.js
+++ src/community.js
@@ -20,57 +20,57 @@ export const community = req => ({
   //---- COMMENTS ----//
 
   getComments(context, itemId, sorting) {
-    return req.get(routes.comments()).query({ context, itemId, sorting })
+    return req.community.get(routes.comments()).query({ context, itemId, sorting })
   },
 
   createComments(comment) {
-    return req.post(routes.comments(), comment)
+    return req.community.post(routes.comments(), comment)
   },
 
   editComment(commentId, body) {
-    return req.put(routes.comment(commentId), { commentId, body })
+    return req.community.put(routes.comment(commentId), { commentId, body })
   },
 
   deleteComment(commentId) {
-    return req.delete(routes.comment(commentId))
+    return req.community.delete(routes.comment(commentId))
   },
 
   //---- REVIEWS ----//
 
   getReviews(context, itemId, sorting) {
-    return req.get(routes.reviews()).query({ context, itemId, sorting })
+    return req.community.get(routes.reviews()).query({ context, itemId, sorting })
   },
 
   createReview(review) {
-    return req.post(routes.reviews(), review)
+    return req.community.post(routes.reviews(), review)
   },
 
   editReview(reviewId, body, rating) {
-    return req.put(routes.review(reviewId), { body, rating })
+    return req.community.put(routes.review(reviewId), { body, rating })
   },
 
   deleteReview(reviewId) {
-    return req.delete(routes.review(reviewId))
+    return req.community.delete(routes.review(reviewId))
   },
 
   //---- VOTES ----//
 
   vote(vote) {
-    return req.post(routes.vote(), vote)
+    return req.community.post(routes.vote(), vote)
   },
 
   getLikers(type, itemId) {
-    return req.get(routes.likers()).query({ type, itemId })
+    return req.community.get(routes.likers()).query({ type, itemId })
   },
 
   getUserBlacklistStatus() {
-    return req.get(routes.blacklistStatus())
+    return req.community.get(routes.blacklistStatus())
   },
 
   //---- ACTIVITY ----//
 
   onProductInstall(productId) {
-    return req.post(routes.onProductInstall(), { productId })
+    return req.community.post(routes.onProductInstall(), { productId })
   },
 
 })
