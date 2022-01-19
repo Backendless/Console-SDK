6d3a705b18dd69f909eb47413b4729dde19167ae
viktor.liablin <L9blin@gmail.com>
2022-01-19 14:28:43 +0200

    Visualizations

--- src/visualizations.js
+++ src/visualizations.js
@@ -1,19 +1,24 @@
-import urls from './urls'
+import { prepareRoutes } from './utils/routes'
+
+const routes = prepareRoutes({
+  visualizations: '/:appId/console/visualizations',
+  visualization: '/:appId/console/visualizations/:visualizationId'
+})
 
 export default req => ({
   getVisualizations(appId) {
-    return req.get(`${urls.appConsole(appId)}/visualizations`)
+    return req.get(routes.visualizations(appId))
   },
 
   createVisualization(appId, visualizationId, visualization) {
-    return req.post(`${urls.appConsole(appId)}/visualizations/${ visualizationId }`, visualization)
+    return req.post(routes.visualization(appId, visualizationId), visualization)
   },
 
   updateVisualization(appId, visualizationId, visualization) {
-    return req.put(`${urls.appConsole(appId)}/visualizations/${ visualizationId }`, visualization)
+    return req.put(routes.visualization(appId, visualizationId), visualization)
   },
 
   deleteVisualizations(appId, visualizationIds) {
-    return req.delete(`${urls.appConsole(appId)}/visualizations`).query({ visualizationIds })
+    return req.delete(routes.visualizations(appId)).query({ visualizationIds })
   },
 })
