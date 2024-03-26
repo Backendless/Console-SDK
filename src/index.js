016ddec2a742aab8dad9279b1976fddff2091e16
Vladimir Upirov <4648606+Valodya@users.noreply.github.com>
2024-03-26 09:11:03 +0200

    - add API for Quick Apps

--- src/index.js
+++ src/index.js
@@ -44,6 +44,7 @@ import uiBuilder from './ui-builder'
 import chartBuilder from './chart-builder'
 import visualizations from './visualizations'
 import consolePreview from './console-preview'
+import quickApps from './quick-apps'
 
 import { community } from './community'
 import { marketplace } from './marketplace'
@@ -208,6 +209,7 @@ const createClient = (serverUrl, authKey, options) => {
     visualizations      : visualizations(request),
     initialQuestionnaire: initialQuestionnaire(request),
     consolePreview      : consolePreview(request),
+    quickApps           : quickApps(request),
   }
 }
 
