f0a402ed33d0fe016b016f34c951a172927dba6a
macbookpro <sergey.androsov@backendlessmail.com>
2024-05-14 11:28:18 +0300

    UIB-14 -Ui builder should able configure settings for PWA

--- src/ui-builder.js
+++ src/ui-builder.js
@@ -30,6 +30,8 @@ const routes = prepareRoutes({
   containerCustomHeadContent: '/:appId/console/ui-builder/containers/:containerName/custom-head-content',
   containerExternalLibraries: '/:appId/console/ui-builder/containers/:containerName/external-libraries',
   containerDefaultI18n      : '/:appId/console/ui-builder/containers/:containerName/i18n',
+  containerPWASettings      : '/:appId/console/ui-builder/containers/:containerName/pwa-settings',
+  containerPWAIcon          : '/:appId/console/ui-builder/containers/:containerName/pwa-icon',
 
   removedContainers: '/:appId/console/ui-builder/removed-containers',
   removedContainer : '/:appId/console/ui-builder/removed-containers/:containerName',
@@ -262,6 +264,14 @@ export default req => ({
 
   updateContainerExternalLibraries(appId, containerName, externalLibraries) {
     return req.put(routes.containerExternalLibraries(appId, containerName), externalLibraries)
+  },
+
+  updateContainerPWASettings(appId, containerName, PWASettings) {
+    return req.put(routes.containerPWASettings(appId, containerName), PWASettings)
+  },
+
+  updateContainerPWAIcon(appId, containerName, file) {
+    return req.put(routes.containerPWAIcon(appId, containerName)).form({ file })
   },
 
   //-- SETTINGS -----//
