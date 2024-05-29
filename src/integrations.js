5049c55bad0de852491eb4de926be23b5f27b678
karyna <kharinakaryna@gmail.com>
2024-05-29 12:18:31 +0300

    IN-23 - Make sure web console uses integration routes for the AI

--- src/integrations.js
+++ src/integrations.js
@@ -1,15 +1,19 @@
 import urls from './urls'
 
 export default req => ({
-  loadOpenAIData(appId) {
-    return req.get(urls.integrations(appId))
+  loadOpenAIData(appId, dataId) {
+    return req.get(urls.integrations(appId, dataId))
   },
 
   saveOpenAIData(appId, aiData) {
     return req.post(urls.integrations(appId), aiData)
   },
 
-  deleteOpenAIData(appId) {
-    return req.delete(urls.integrations(appId))
+  updateOpenAIData(appId, dataId, configData) {
+    return req.put(urls.integrations(appId, dataId), configData)
+  },
+
+  deleteOpenAIData(appId, dataId) {
+    return req.delete(urls.integrations(appId, dataId))
   },
 })
