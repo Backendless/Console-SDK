d9ec5a907f30dd593de67dfa20e8f05d3ce73ded
Sergey Androsov <62656978+andrsvsrg@users.noreply.github.com>
2024-07-29 23:21:27 +0300

    CS-40 - Console top level navigation changes (#393)
    * CS-40 - Console top level navigation changes
    
    * - refactor
    
    ---------
    
    Co-authored-by: macbookpro <sergey.androsov@backendlessmail.com>
    Co-authored-by: Vladimir Upirov <4648606+Valodya@users.noreply.github.com>

--- src/apps.js
+++ src/apps.js
@@ -1,59 +1,96 @@
-import urls from './urls'
+import { prepareRoutes } from './utils/routes'
+
+const routes = prepareRoutes({
+  apps         : '/console/applications',
+  appFromZip   : '/console/applications/from-zip',
+  appFromZipURL: '/console/applications/from-zip-url',
+
+  app          : '/:appId/console/application',
+  appDevOptions: '/:appId/console/developer/options',
+
+  appReset       : '/:appId/console/appreset',
+  appClone       : '/:appId/console/cloneApp',
+  appCloneProcess: '/:appId/console/cloneApp/:processId',
+  appTransfer    : '/:appId/console/application/transfer',
+
+  appsInfo   : '/console/apps-info',
+  appInfo    : '/:appId/console/app-info',
+  appInfoLogo: '/:appId/console/app-info/logos',
+
+  suggestedGeneratedDomains: '/console/applications/suggested-generated-domains'
+})
 
 export default req => ({
   createApp(app, query) {
-    return req.post('/console/applications', app).query(query)
+    return req.post(routes.apps(), app).query(query)
   },
 
   createAppFromZIP(app) {
-    return req.post('/console/applications/from-zip', app)
+    return req.post(routes.appFromZip(), app)
   },
 
   createAppFromZipUrl(data) {
-    return req.post('/console/applications/from-zip-url', data)
+    return req.post(routes.appFromZipURL(), data)
   },
 
   getApps(zone) {
-    return req.get('/console/applications').query({ zone })
+    return req.get(routes.apps()).query({ zone })
   },
 
   resetApp(appId, resets) {
-    return req.post(`${urls.appConsole(appId)}/appreset`, resets)
+    return req.post(routes.appReset(appId), resets)
   },
 
   renameApp(appId, appName) {
-    return req.put(`${urls.appConsole(appId)}/application`, { appName })
+    return req.put(routes.app(appId), { appName })
   },
 
   deleteApp(appId) {
-    return req.delete(`${urls.appConsole(appId)}/application`)
+    return req.delete(routes.app(appId))
   },
 
   cloneApp(appId, newApp) {
-    return req.post(`${urls.appConsole(appId)}/cloneApp`, newApp)
+    return req.post(routes.appClone(appId), newApp)
   },
 
   generateAppZIP(appId) {
-    return req.post(`${urls.appConsole(appId)}/application/transfer`)
+    return req.post(routes.appTransfer(appId))
   },
 
   getCloningAppStatus(appId, processId) {
-    return req.get(`${urls.appConsole(appId)}/cloneApp/${processId}`)
+    return req.get(routes.appCloneProcess(appId, processId))
   },
 
-  updateAppInfo(appId, info) {
-    return req.post(urls.appInfo(appId), info)
+  loadAppsMenuItems() {
+    return req.get('/console/applications/menu-items')
+  },
+
+  loadAppFavorites(appId, devId) {
+    return req.get('/console/applications/app-favorites').query({ appId, devId })
   },
 
   loadAppsInfo(appsIds) {
-    return req.get('/console/apps-info').query({ appsIds })
+    return req.get(routes.appsInfo()).query({ appsIds })
+  },
+
+  updateAppInfo(appId, info) {
+    return req.post(routes.appInfo(appId), info)
   },
 
   updateAppLogo(appId, logo) {
-    return req.post(`${urls.appInfo(appId)}/logos`, logo)
+    return req.post(routes.appInfoLogo(appId), logo)
   },
 
   generateSubdomains(zone) {
-    return req.get('/console/applications/suggested-generated-domains').query({ zone })
-  }
+    return req.get(routes.suggestedGeneratedDomains()).query({ zone })
+  },
+
+  getAppDevOptions(appId) {
+    return req.get(routes.appDevOptions(appId))
+  },
+
+  updateAppDevOptions(appId, options) {
+    return req.put(routes.appDevOptions(appId), options)
+  },
+
 })
