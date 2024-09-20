df11b94d4156bbb0d9f780edcddd372f86614917
macbookpro <sergey.androsov@backendlessmail.com>
2024-09-20 16:28:19 +0300

    AUTO-724 - Add connections page, actions

--- src/oauth2-integration.js
+++ src/oauth2-integration.js
@@ -3,6 +3,7 @@ import { prepareRoutes } from './utils/routes'
 const routes = prepareRoutes({
   integrationConnections      : '/api/node-server/manage/app/:appId/integration/connections',
   integrationConnectionById   : '/api/node-server/manage/app/:appId/integration/connections/:connectionId',
+  integrationConnectionName   : '/api/node-server/manage/app/:appId/integration/connections/:connectionId/name',
   integrationConnectionsUsages: '/api/node-server/manage/app/:appId/integration/connections/usages',
   integrationConnectionURL    : '/api/node-server/manage/app/:appId/integration/connection/oauth/url',
 })
@@ -14,6 +15,10 @@ export default req => ({
 
   deleteIntegrationConnectionById(appId, connectionId) {
     return req.delete(routes.integrationConnectionById(appId, connectionId))
+  },
+
+  updateIntegrationConnectionName(appId, connectionId, updatedName) {
+    return req.put(routes.integrationConnectionName(appId, connectionId), { updatedName })
   },
 
   getConnectionsUsages(appId) {
