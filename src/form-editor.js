7bbba55e0d24e1268fb73e242ecff4a9fb8e0d5d
serhiy.melnychuk <serhiy.melnychuk@backendlessmail.com>
2025-04-04 13:54:37 +0300

    use node server for form editor api

--- src/form-editor.js
+++ src/form-editor.js
@@ -1,31 +1,39 @@
-import urls from './urls'
+import { prepareRoutes } from './utils/routes'
+
+const routes = prepareRoutes({
+  forms     : '/api/node-server/manage/app/:appId/form-editor',
+  createForm: '/api/node-server/manage/app/:appId/form-editor/form',
+  updateForm: '/api/node-server/manage/app/:appId/form-editor/:formName',
+  deleteForm: '/api/node-server/manage/app/:appId/form-editor/:formName',
+  renameForm: '/api/node-server/manage/app/:appId/rename-form',
+})
 
 export default req => ({
 
   getForms(appId) {
-    return req.get(`${urls.appConsole(appId)}/form-editor`)
+    return req.get(routes.forms(appId))
   },
 
   createForm(appId, formSource, formTarget) {
-    return req.post(`${urls.appConsole(appId)}/form-editor/form`, {
+    return req.post(routes.createForm(appId), {
       formSource,
       formTarget
     })
   },
 
   updateForm(appId, formSource, formTarget) {
-    return req.put(`${urls.appConsole(appId)}/form-editor/form/${formSource.name}`, {
+    return req.put(routes.updateForm(appId, formSource.name), {
       formSource,
       formTarget
     })
   },
 
   deleteForm(appId, formName) {
-    return req.delete(`${urls.appConsole(appId)}/form-editor/form/${formName}`)
+    return req.delete(routes.deleteForm(appId, formName))
   },
 
   renameForm(appId, oldFormName, newFormName) {
-    return req.put(`${urls.appConsole(appId)}/form-editor/rename-form`, {
+    return req.put(routes.renameForm(appId), {
       oldFormName,
       newFormName
     })
